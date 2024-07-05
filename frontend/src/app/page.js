98914299d25b37721fff9edd25be679b61be2ed0
0adiy <adityaruva@gmail.com>
2024-07-05 13:22:07 +0530

    added clerk auth

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,10 @@
 import Tree from "@/components/Tree";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between bg-base-300 w-full'>
       <Tree />
     </main>
   );
 }
-
---
+++ frontend/src/app/user/page.jsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function User() {
+  return <h1>user</h1>;
+}
